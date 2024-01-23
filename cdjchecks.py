64c6a522091a97af8c0e65666b332e06bab8ebe9
Vesper <3692034+noodlecollie@users.noreply.github.com>
2024-01-23 12:43:21 +0000

    Added CDJ file check script

---
+++ cdjchecks.py
@@ -0,0 +1,119 @@
+import argparse
+import os
+import sys
+from lib import config
+
+SCRIPT_DIR = os.path.realpath(os.path.dirname(__file__))
+
+WELL_SUPPORTED_EXTENSIONS = [
+	".mp3",
+	".flac",
+	".wav",
+	".aiff"
+]
+
+EXTENSIONS_TO_CHECK = WELL_SUPPORTED_EXTENSIONS + [
+	".ogg",
+	".wma"
+]
+
+def parseArgs():
+	parser = argparse.ArgumentParser(
+		"cdjchecks",
+		description="Runs validation checks on a directory to search for potential library compatibility issues with "
+		"CDJ controllers."
+	)
+
+	parser.add_argument(
+		"-l",
+		"--list-files",
+		action="store_true",
+		help="Prints lists of files that fail each validation check."
+	)
+
+	parser.add_argument(
+		"dirs",
+		nargs="*",
+		help="One or more directories to scan recursively. Relative paths are treated as being relative to the "
+		"library config file. If no paths are specified, the configured DJ directory is used."
+	)
+
+	return parser.parse_args()
+
+def loadConfig():
+	return config.Config(os.path.join(SCRIPT_DIR, "config.json"))
+
+def computePaths(configFile:config.Config, paths:list):
+	outPaths = []
+
+	for path in paths:
+		if os.path.isabs(path):
+			outPaths.append(path)
+		else:
+			outPaths.append(os.path.join(configFile.getBaseDirPath(), path))
+
+	return outPaths
+
+def addResult(results:dict, key:str, value:str):
+	if key not in results:
+		results[key] = [value]
+	else:
+		results[key].append(value)
+
+def main():
+	args = parseArgs()
+
+	configFile = loadConfig()
+	paths = computePaths(configFile, args.dirs) if args.dirs else [configFile.getDJDirPath()]
+
+	filesToProcess = []
+
+	for inputPath in paths:
+		print("Checking:", inputPath)
+
+		if not os.path.isdir(inputPath):
+			continue
+
+		for root, dirs, files in os.walk(inputPath):
+			for file in files:
+				if not os.path.splitext(file)[1] in EXTENSIONS_TO_CHECK:
+					continue
+
+				filesToProcess.append(os.path.realpath(os.path.join(root, file)))
+
+	print(f"Found {len(filesToProcess)} files to validate")
+
+	if not filesToProcess:
+		sys.exit(0)
+
+	results = {}
+
+	for file in filesToProcess:
+		extension = os.path.splitext(file)[1]
+
+		if extension not in WELL_SUPPORTED_EXTENSIONS:
+			addResult(results, "File format not supported", file)
+
+		if extension != ".mp3":
+			addResult(results, "Not an MP3", file)
+
+	if not results:
+		print("All files validated")
+		sys.exit(0)
+
+	print("Results:")
+
+	for key in results:
+		values = results[key]
+		print(f"  {key}: {len(values)} files")
+
+		if args.list_files:
+			for value in values:
+				print(f"    {value}")
+
+	sys.exit(1)
+
+if __name__ != "__main__":
+	raise RuntimeError("Expected file to be run as a script")
+
+main()
---
+++ lib/config.py
@@ -0,0 +1,54 @@
+import json
+import os
+
+class Config:
+	def __init__(self, configFilePath:str) -> None:
+		self.__baseDirPath:str = os.path.dirname(configFilePath)
+		self.__personalDir:str = ""
+		self.__djDir:str = ""
+		self.__draftDir:str = ""
+
+		self.__loadJSON(configFilePath)
+
+	def getBaseDirPath(self) -> str:
+		return self.__baseDirPath
+
+	def getPersonalDir(self) -> str:
+		return self.__personalDir
+
+	def getPersonalDirPath(self) -> str:
+		return self.__makePath(self.__personalDir)
+
+	def getDJDir(self) -> str:
+		return self.__djDir
+
+	def getDJDirPath(self) -> str:
+		return self.__makePath(self.__djDir)
+
+	def getDraftDir(self) -> str:
+		return self.__draftDir
+
+	def getDraftDirPath(self) -> str:
+		return self.__makePath(self.__draftDir)
+
+	def __makePath(self, path:str):
+		return path if os.path.isabs(path) else os.path.join(self.getBaseDirPath(), path)
+
+	def __loadJSON(self, configFilePath:str) -> None:
+		with open(configFilePath, "r") as inFile:
+			contents = json.load(inFile)
+
+			self.__personalDir = self.__tryRead(contents, "personal", str)
+			self.__djDir = self.__tryRead(contents, "dj", str)
+			self.__draftDir = self.__tryRead(contents, "draft", str)
+
+	def __tryRead(self, jsonObj:dict, prop:str, valueType:type):
+		if prop not in jsonObj:
+			raise KeyError(f'Config JSON did not contain required property "{prop}"')
+
+		value = jsonObj[prop]
+
+		if type(value) is not valueType:
+			raise TypeError(f'Expected config JSON property "{prop}" to be {valueType.__name__}, but provided value was {type(value).__name__}')
+
+		return value
