0937b2d6466e12b8e15f6d12766434fcc635ca3b
Vesper <3692034+noodlecollie@users.noreply.github.com>
2024-01-26 08:03:14 +0000

    Added placeholders for future validation checks

--- lib/validation.py
+++ lib/validation.py
@@ -9,14 +9,22 @@ WELL_SUPPORTED_FORMATS = [
 
 ALL_MEDIA_FORMATS = WELL_SUPPORTED_FORMATS + [
 	".ogg",
-	".wma"
+	".wma",
+	".m4a"
 ]
 
+DOES_NOT_EXIST = "File does not exist"
 NOT_AN_MP3 = "Not an MP3"
 UNSUPPORTED_FORMAT = "Unsupported file format"
+OVER_TEN_MINUTES_LONG = "Over 10 minutes long"
+MP3_LESS_THAN_320K = "MP3 bitrate less than 320k"
+MISSING_BASIC_METADATA = "Missing basic metadata"
 
 def validateFile(filePath:str):
 	validationErrors = []
+
+	if not os.path.isfile(filePath):
+		validationErrors.append(DOES_NOT_EXIST)
 
 	extension = os.path.splitext(filePath)[1]
 
