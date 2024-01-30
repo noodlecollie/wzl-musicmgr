137aedaecd3e19520d0ed632df78e867ee66033a
Jonathan Poncelet <3692034+noodlecollie@users.noreply.github.com>
2024-01-30 09:21:54 +0000

    Added utility to list ID3 tags

---
+++ listtags.py
@@ -0,0 +1,28 @@
+import argparse
+from lib import id3
+
+def parseArgs():
+	parser = argparse.ArgumentParser(
+		"listtage",
+		description="Lists metadata tags on the specified file."
+	)
+
+	parser.add_argument(
+		"file",
+		nargs=1,
+		help="File to dump tags from."
+	)
+
+	return parser.parse_args()
+
+def main():
+	args = parseArgs()
+	tags = id3.dump_tag_string_dict(args.file[0])
+
+	for key in tags:
+		print(f"{key}: {tags[key]}")
+
+if __name__ != "__main__":
+	raise RuntimeError("Expected file to be run as a script")
+
+main()
